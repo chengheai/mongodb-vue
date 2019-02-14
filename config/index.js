bd8ce0498b4ec955be955da28970be47e20daf9c
chengheai <842385139@qq.com>
2019-02-14 11:18:48 +0800

    🌚fix change some

--- config/index.js
+++ config/index.js
@@ -12,7 +12,8 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:3000',
+        // target: 'http://localhost:3000',
+        target: 'http://144.34.160.26:3000',
         changeOrigin: true,
       }
     },
