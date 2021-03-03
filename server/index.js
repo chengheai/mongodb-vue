5327158f0c717412ac2b44b8a9548bb34034d419
chengheai <chengheai@021.com>
2021-03-03 15:12:09 +0800

    rm useless pugins

--- server/index.js
+++ server/index.js
@@ -3,10 +3,9 @@ const hero = require("../routers/hero");
 const user = require("../routers/user");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cors = require('cors');
 
 // 这一句是连接上数据库
-mongoose.connect("mongodb://localhost:27017/lolTest", {
+mongoose.connect("mongodb://172.21.15.44:27017/lolTest", {
   useNewUrlParser: true
 });
 
@@ -14,23 +13,10 @@ mongoose.connect("mongodb://localhost:27017/lolTest", {
 
 const app = express();
 
-app.use(cors({
-  origin: ['http://localhost:8080'] // 指定接受的地址
-}))
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api", hero);
 app.use("/api", user);
-
-// app.all('*', function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-//   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-//   res.header("X-Powered-By", ' 3.2.1')
-//   res.header("Content-Type", "application/json;charset=utf-8");
-//   next();
-// });
 
 // 监听端口
 app.listen(50001);
