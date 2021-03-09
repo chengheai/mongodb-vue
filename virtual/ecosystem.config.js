c114d437b5dc76eac02a34f28ff241392adee42e
root <root@localhost.localdomain>
2021-03-09 17:17:32 +0800

    add virtual

---
+++ virtual/ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: "sse-vue",
+      cwd: '/root/code/sse-vue-demo/server',
+      script: "/root/code/sse-vue-demo/server/index.js"
+    },
+    {
+      name: "mongodb-vue",
+      cwd: '/root/code/mongodb-vue/server',
+      script: "/root/code/mongodb-vue/server/index.js"
+    },
+    {
+      name: "sse-huidong",
+      cwd: '/root/code/sse-huidong/server',
+      script: "/root/code/sse-huidong/server/index.js"
+    },
+  ]
+}
+
